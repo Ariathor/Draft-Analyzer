affd8a0d8546f80f067ff7231af093fdbe9db5e3
Ioannis M <35879be9@opayq.com>
2015-07-13 23:24:59 +0200

    Revert "Revert "Draft Analyzer""
    This reverts commit 0e0e18e44236697df8c7ad588fd50785a65e6f74.

---
+++ Draft Analyzer.py
@@ -0,0 +1,27 @@
+__author__ = 'Ioannis'
+# Remember to set verboseFlag in config_handler to False for distribution
+
+import threading
+import queue
+
+import download_data
+import AH_data_handler
+import gui_with_queue
+import config_handler
+
+
+def initialize(exQueue):
+    download_data.main()
+    AH_data_handler.main()
+    gui = exQueue.get()                     # Currently used instance of the Gui class
+    gui.reload_stats()                      # Possibly problematic if initialize finishes before gui is created
+
+    if config_handler.verboseFlag:
+        print("Initialization Complete")
+
+# THREADING WITH GUIS - needs fixing, why is gui not in the main thread
+if __name__ == '__main__':
+    exQueue = queue.Queue(maxsize=1)
+    threading.Thread(target=initialize, args=(exQueue,)).start()
+    gui_with_queue.main(exQueue)
+
